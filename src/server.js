6f6d47d57059ff8e6ebd952ff1b4ea1f300edbd3
youngdoo <chlduden11@snu.ac.kr>
2022-01-04 14:57:51 +0900

    ~lec3.5 MiddleWare

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-	return res.send("asdfasdfa");
-});
-
-app.get("/login", (req, res) => {
-	return res.send("Longin here.");
-});
+app.get(
+	"/",
+	(req, res, next) => {
+		console.log(`going to http://localhost:${PORT}${req.url}`);
+		next();
+	},
+	(req, res, next) => {
+		console.log("as;ldkfjsd;lkfjasdf;lkj");
+		next();
+	},
+	(req, res) => {
+		return res.end();
+	}
+);
 
 const handleListening = () =>
 	console.log(`Server listening on port http://localhost:${PORT}`);
