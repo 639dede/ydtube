386a40d3e29042768f5f4d1c18ae5f63b497c228
youngdoo <chlduden11@snu.ac.kr>
2022-01-04 11:13:24 +0900

    ~lec3.4

--- src/server.js
+++ src/server.js
@@ -4,7 +4,13 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", () => console.log("Somebody.."));
+app.get("/", (req, res) => {
+	return res.send("asdfasdfa");
+});
+
+app.get("/login", (req, res) => {
+	return res.send("Longin here.");
+});
 
 const handleListening = () =>
 	console.log(`Server listening on port http://localhost:${PORT}`);
