0297354f0077c9cc9894d5d1024eb1d7bdc5a9c2
youngdoo <chlduden11@snu.ac.kr>
2022-01-05 17:48:31 +0900

    MiddleWare

--- src/server.js
+++ src/server.js
@@ -4,20 +4,34 @@ const PORT = 4000;
 
 const app = express();
 
-app.get(
-	"/",
-	(req, res, next) => {
-		console.log(`going to http://localhost:${PORT}${req.url}`);
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+};
+
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if (url === "/protected") {
+		return res.send("<h1>Not Allowed<h1>");
+	} else {
+		console.log("Allowed, you may continue.");
 		next();
-	},
-	(req, res, next) => {
-		console.log("as;ldkfjsd;lkfjasdf;lkj");
-		next();
-	},
-	(req, res) => {
-		return res.end();
 	}
-);
+};
+
+const handleHome = (req, res) => {
+	return res.send("dafdsf");
+};
+
+const handleProtected = (req, res) => {
+	return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
 	console.log(`Server listening on port http://localhost:${PORT}`);
