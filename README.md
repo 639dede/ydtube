424c51bb32ede99587a678d254f9b92c537c5b2f
youngdoo <chlduden11@snu.ac.kr>
2022-01-06 22:34:30 +0900

    lec 4.2 Routers

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ydtube-reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,37 +1,23 @@
 import express from "express";
 
+import morgan from "morgan";
+
+import globalRouter from "./routers/globalRouter";
+
+import userRouter from "./routers/userRouter";
+
+import videoRouter from "./routers/videoRouter";
+
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-	console.log(`${req.method} ${req.url}`);
-	next();
-};
-
-const privateMiddleware = (req, res, next) => {
-	const url = req.url;
-	if (url === "/protected") {
-		return res.send("<h1>Not Allowed<h1>");
-	} else {
-		console.log("Allowed, you may continue.");
-		next();
-	}
-};
-
-const handleHome = (req, res) => {
-	return res.send("dafdsf");
-};
-
-const handleProtected = (req, res) => {
-	return res.send("Welcome to the private lounge.");
-};
+const logger = morgan("common");
 
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
 	console.log(`Server listening on port http://localhost:${PORT}`);
