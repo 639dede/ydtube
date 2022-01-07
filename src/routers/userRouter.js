dfbd451d6d71c3ad2f1d9c396777abcdeda61531
youngdoo <chlduden11@snu.ac.kr>
2022-01-07 23:53:05 +0900

    lec 4.5 Router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { edit, remove } from "../controllers/userController";
+
 const userRouter = express.Router();
 
-const handleEdituser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEdituser);
+userRouter.get("/edit", edit);
+userRouter.get("/delete", remove);
 
 export default userRouter;
