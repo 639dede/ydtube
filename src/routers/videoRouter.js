7aff3e007666ffe1f6852baa57b162fbf7baf9ed
youngdoo <chlduden11@snu.ac.kr>
2022-01-12 08:31:15 +0900

    lec 6.4 FakeDatabase done

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,13 +4,14 @@ import {
 	watch,
 	getEdit,
 	postEdit,
-	deleteVideo,
-	upload,
+	getUpload,
+	postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/:id(\\d+)").get(watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
