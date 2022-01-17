9cfedad73fc0831ec6c04d2780110ac43ca4ef2d
youngdoo <chlduden11@snu.ac.kr>
2022-01-17 20:12:02 +0900

    lec 6.20 Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,26 @@
 import Video from "../models/Video";
 
-//Video.find({}, (error, videos) => {});
-
 export const home = async (req, res) => {
 	const videos = await Video.find({});
 	return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
 	const { id } = req.params;
-	return res.render("watch", { pageTitle: "Watching" });
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.render("404", { pageTitle: "Video Not Found." });
+	}
+	return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
 	const { id } = req.params;
-	return res.render("edit", { pageTitle: `Editing` });
+	const video = await Video.findById(id);
+	if (!video) {
+		return res.render("404", { pageTitle: "Video Not Found." });
+	}
+	return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
@@ -27,7 +33,20 @@ export const getUpload = (req, res) => {
 	return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-	const { title } = req.body;
-	return res.redirect("/");
+export const postUpload = async (req, res) => {
+	const { title, description, hashtags } = req.body;
+	try {
+		await Video.create({
+			title,
+			description,
+			hashtags: hashtags.split(",").map((word) => `#${word}`),
+		});
+		return res.redirect("/");
+	} catch (error) {
+		console.log(error);
+		return res.render("upload", {
+			pageTitle: "Upload Video",
+			errorMessage: error._message,
+		});
+	}
 };
