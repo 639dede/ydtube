bc0b14fe74da38be956519718a6e41dfebe0e8b3
youngdoo <chlduden11@snu.ac.kr>
2022-01-23 19:06:27 +0900

    lec 7.11 loggin in

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 import "./db";
 import "./models/Video";
 import express from "express";
-
 import morgan from "morgan";
+import session from "express-session";
 
 import rootRouter from "./routers/rootRouter";
 
 import userRouter from "./routers/userRouter";
 
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -18,6 +19,16 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: "Hello",
+		resave: true,
+		saveUninitialized: true,
+	})
+);
+
+app.use(localsMiddleware);
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
