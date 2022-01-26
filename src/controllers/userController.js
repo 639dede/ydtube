02077cd1267fe3f6092073f9147d397e82a61cca
youngdoo <chlduden11@snu.ac.kr>
2022-01-26 18:42:01 +0900

    lec 8.3 EditProfile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
+import { token } from "morgan";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
@@ -44,7 +46,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
 	const { username, password } = req.body;
 	const pageTitle = "Login";
-	const user = await User.findOne({ username });
+	const user = await User.findOne({ username, socialOnly: false });
 	if (!user) {
 		return res.status(400).render("login", {
 			pageTitle,
@@ -63,10 +65,121 @@ export const postLogin = async (req, res) => {
 	return res.redirect("/");
 };
 
-export const edit = (req, res) => res.send("Edit User");
+export const startGithubLogin = (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/authorize";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		allow_signup: false,
+		scope: "read:user user:email",
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+	return res.redirect(finalUrl);
+};
 
-export const remove = (req, res) => res.send("Remove User");
+export const finishGithubLogin = async (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/access_token";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		client_secret: process.env.GH_SECRET,
+		code: req.query.code,
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+	const tokenRequest = await (
+		await fetch(finalUrl, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+		})
+	).json();
+	if ("access_token" in tokenRequest) {
+		const { access_token } = tokenRequest;
+		const apiUrl = "https://api.github.com";
+		const userData = await (
+			await fetch(`${apiUrl}/user`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+		console.log(userData);
+		const emailData = await (
+			await fetch(`${apiUrl}/user/emails`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+		const emailObj = emailData.find(
+			(email) => email.primary === true && email.verified === true
+		);
+		let user = await User.findOne({ email: emailObj.email });
+		if (!user) {
+			user = await User.create({
+				avatarUrl: userData.avatar_url,
+				name: userData.name,
+				username: userData.login,
+				email: emailObj.email,
+				socialOnly: true,
+				password: "",
+				loaction: userData.location,
+			});
+		}
+		req.session.loggedIn = true;
+		req.session.user = user;
+		return res.redirect("/");
+	} else {
+		return res.redirect("/login");
+	}
+};
 
-export const logout = (req, res) => res.send("Log out");
+export const getEdit = (req, res) => {
+	return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+
+export const postEdit = async (req, res) => {
+	const {
+		session: {
+			user: { _id, email: sessionEmail, username: sessionUsername },
+		},
+		body: { name, email, username, location },
+	} = req;
+	let serachParam = [];
+	if (sessionEmail !== email) {
+		serachParam.push({ email });
+	}
+	if (sessionUsername !== username) {
+		serachParam.push({ username });
+	}
+	console.log(serachParam);
+	if (serachParam.length > 0) {
+		const foundUser = await User.findOne({ $or: serachParam });
+		if (foundUser && foundUser._id.toString() !== _id) {
+			return res.status(400).render("edit-profile", {
+				pageTitle: "Edit Profile",
+				errorMessage: "This username/email is already taken.",
+			});
+		}
+	}
+	const updatedUser = await User.findByIdAndUpdate(
+		_id,
+		{
+			name,
+			email,
+			username,
+			location,
+		},
+		{ new: true }
+	);
+	req.session.user = updatedUser;
+	return res.redirect("/users/edit");
+};
+
+export const logout = (req, res) => {
+	req.session.destroy();
+	return res.redirect("/");
+};
 
 export const see = (req, res) => res.send("See User");
